ddba7da4f0f171ae6745de88332f382a869373ba
Mariana Rodrigues <marianarodriguesdesantana@gmail.com>
2022-06-05 16:33:25 -0300

    is solvable?

--- AEstrelaImp.py
+++ AEstrelaImp.py
@@ -1,10 +1,10 @@
+from numpy import append
 from Posicao import Posicao
 from AEstrela import AEstrela
 from QuebraCabeca import QuebraCabeca
 from QuebraCabecaImp import QuebraCabecaImp
 
 def solucionavel(puzzle):
-
     puzzleList = [] # matriz do tabuleiro no formato de lista, para verificar todas as combinacoes
     for l in puzzle:
         puzzleList += l
@@ -17,37 +17,42 @@ def solucionavel(puzzle):
     return (cont % 2 == 0) #se o numero de inversoes for par, tem solucao
 
 class AEstrelaImp(AEstrela):
+    # @param qc - Quebra - cabeca com o estado inicial
+    # @return lista de objeto da classe Posicao com os movimentos a serem realizados
     def getSolucao(self, qc):
 
         if (not solucionavel(qc.getTab())):
             print("Sem solução")
-            return 
+            return []
 
         configsVisitadas = []
-        posVazia = []
-        configsVisitadas.append(qc.hashCode())
+        posVazia = [] #lista de objeto da classe Posicao com os movimentos a serem realizados
+        configs = [] #cada configuração de tabuleiro tera um custo total(f) e a propria tabel associada
+        
         g = 1 # custo da distancia entre os nós - inicialmente 1 até o próximo nó (altura da árvore)
         f = qc.getValor() + g # custo total de um nó até ao outro
-        configs = [[ f, g, qc.getTab() ]] #cada configuração de tabuleiro tera essas informacoes associadas
+        
+        configs.append([f, qc.getTab()]) 
+        configsVisitadas.append(qc.hashCode()) #marcamos a config inicial do tabuleiro como visitada
 
         while(qc.getTab() != qc.tabGabarito): #enquanto nao chegarmos ao resultado
             movimentos = qc.getMovePossiveis() #uma lista de posições de movimentos possiveis
-            vazio = qc.getPosVazio()         #coordenadas da posicao vazia
+            vazio = qc.getPosVazio() #coordenadas da posicao vazia
             
             for destino in movimentos:
                 qc.move(vazio.getLinha(), vazio.getColuna(), destino.getLinha(), destino.getColuna()) #move o vazio
-                tableAux = qc.getTab()
+                tableCurrent = qc.getTab()
                 #calcula o caminho
                 if qc.hashCode() not in configsVisitadas: #se ainda nao verificamos essa config de tabuleiro
                     configsVisitadas.append(qc.hashCode()) #marcamos como verificada, adicionando ao vetor de visitados
                     f = qc.getValor() + (g +1) #mais um nivel na arvore de solucoes
-                    configs.append([f, g , tableAux])
+                    configs.append([f , tableCurrent])
                 #retorna pra posicao
                 qc.move(destino.getLinha(), destino.getColuna(), vazio.getLinha(), vazio.getColuna())
 
             configs.pop(0)
-            configs.sort() # ordena crescentemente de acordo com o valor do custo total do caminho
-            qc.setTab(configs[0][2]) # escolhemos sempre o no com menor custo e exploramos ele
-            posVazia.append(qc.getPosVazio())
+            configs.sort() # ordena crescentemente de acordo com o valor do custo total do caminho (f)
+            qc.setTab(configs[0][1]) # escolhemos sempre o no com menor custo e exploramos ele
+            posVazia.append(qc.getPosVazio()) #getPosVazio -  @return objeto Posicao com as coordenadas da posicao vazia
 
         return posVazia   
--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def main():
     instance = AEstrelaImp()
 
     #tab = [[1, 2, 3], [4, QuebraCabecaImp.VAZIO, 5], [6, 7, 8]]
-    #tab = [[7, 2, 1], [3, 4, 8], [6, QuebraCabecaImp.VAZIO, 5]]
-    tab = [[3, QuebraCabecaImp.VAZIO, 7], [2, 8, 1], [6, 4, 5]]
+    tab = [[7, 2, 1], [3, 4, 8], [6, QuebraCabecaImp.VAZIO, 5]]
+    #tab = [[3, QuebraCabecaImp.VAZIO, 7], [2, 8, 1], [6, 4, 5]]
     #tab = [[7, 2, 4], [5, QuebraCabecaImp.VAZIO, 6], [8, 3, 1]]
     #tab = [[8, 1, 2], [QuebraCabecaImp.VAZIO, 4, 3], [7, 6, 5]] # Exemplo sem solução (Not Solvable). Ref: https://www.geeksforgeeks.org/check-instance-8-puzzle-solvable/    
     qc.setTab(tab)
